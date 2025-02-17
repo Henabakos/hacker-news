92570b242bd59a9bccc81e6e5dda90d0b56e521a
Henabakos <henogato9876@gmail.com>
2025-02-17 12:59:49 +0300

    Added heder to the project api

--- utils/api.ts
+++ utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const base_URL = "https://newsapi.org/v2/everything";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
 
-export async function fetchArticles(query: string, page = 1, pageSize = 12) {
+export async function fetchArticles(query: string, page = 1, pageSize = 10) {
   try {
     const response = await axios.get(`${base_URL}`, {
       params: {
